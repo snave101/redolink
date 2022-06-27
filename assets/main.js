19159c15784028580e6dd91632647f775b5ca2d1
snave101 <ronohevans550@gmail.com>
2022-06-27 21:58:42 +0300

    https://github.com/snave101/redolink.git

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,27 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
+const button10 = document.querySelector("#plusTen")
+const indicato = document.querySelector("#number-indicator")
+const dis = document.querySelector("#display-heading")
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+const button15 = document.querySelector("#plusFifteen")
+const inicator = document.querySelector("#number-indicator")
+const dsp = document.querySelector("#display-heading")
+
+button15.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+const button20 = document.querySelector("#plusTwenty")
+const indicatr = document.querySelector("#number-indicator")
+const dip = document.querySelector("#display-heading")
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+})
